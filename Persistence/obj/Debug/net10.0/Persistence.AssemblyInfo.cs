34f80765dea7300a47cf2e4bc2f7f5e06fafc5fb
alenko998 <alen_git@hotmail.com>
2025-12-17 13:54:54 +0100

    Adding a Mediator handler for Editing an Activity

--- Persistence/obj/Debug/net10.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net10.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b44d15ac0a257887bf2a78163b56f5b42fbbb4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34f3def676b7111c4a048be42f687212c30b3c7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
