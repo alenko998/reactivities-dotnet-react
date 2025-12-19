0aa787d0d6c35eb36ee48c6c9f6f46b84f6c3860
alenko998 <alen_git@hotmail.com>
2025-12-19 17:52:20 +0100

    Adding AutoMapper to the project

--- Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34f3def676b7111c4a048be42f687212c30b3c7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34f80765dea7300a47cf2e4bc2f7f5e06fafc5fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
