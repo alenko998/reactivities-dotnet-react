e91ad6078beaf97b64a8dd5cf3ad925f83d2139f
alenko998 <alen_git@hotmail.com>
2025-12-23 21:56:48 +0100

    Using the useState hook

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -8,6 +8,7 @@ import ActivityDashboard from "../../features/activities/dashboard/ActivityDashb
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+  const [editMode, setEditMode] = useState(false)
 
   useEffect(() => {
     axios.get<Activity[]>('https://localhost:5001/api/activities')
@@ -24,12 +25,22 @@ function App() {
     setSelectedActivity(undefined);
   }
 
+  const handleOpenForm = (id?: string) => {
+    if (id) handleSelectActivity(id)
+    else handleCancelSelectActivity();
+    setEditMode(true);
+  }
+  
+  const handleFormClose = () => {
+    setEditMode(false);
+  }
+
   return (
     <Box sx={{bgcolor: '#eeeeee'}}>
       <CssBaseline/>
-      <NavBar/>
+      <NavBar openForm={handleOpenForm}/>
       <Container maxWidth='xl' sx={{mt: 3}}>
-       <ActivityDashboard selectedActivity={selectedActivity} selectActivity={handleSelectActivity} cancelSelectActivity={handleCancelSelectActivity} activities = {activities}/>
+       <ActivityDashboard closeForm={handleFormClose} editMode={editMode} openForm={handleOpenForm} selectedActivity={selectedActivity} selectActivity={handleSelectActivity} cancelSelectActivity={handleCancelSelectActivity} activities = {activities}/>
       </Container>
     </Box>
       
--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -9,17 +9,20 @@ type Props = {
   selectActivity: (id:string) => void;
   cancelSelectActivity: () => void;
   selectedActivity?: Activity;
+  openForm: (id: string) => void;
+  closeForm: () => void;
+  editMode: boolean
 }
 
-export default function ActivityDashboard({activities, selectActivity, cancelSelectActivity, selectedActivity}: Props) {
+export default function ActivityDashboard({openForm, closeForm, editMode, activities, selectActivity, cancelSelectActivity, selectedActivity}: Props) {
   return (
     <Grid2 container spacing={3}>
         <Grid2 size={7}>
           <ActivityList activities={activities} selectActivity={selectActivity} />
         </Grid2>
         <Grid2 size={5}>
-          {selectedActivity && <ActivityDetails activity={selectedActivity} cancelSelectActivity={cancelSelectActivity}/>}
-          <ActivityForm />
+          {selectedActivity && !editMode && <ActivityDetails activity={selectedActivity} cancelSelectActivity={cancelSelectActivity} openForm={openForm} />}
+          { editMode && <ActivityForm closeForm={closeForm} activity={selectedActivity}/> }
         </Grid2>
     </Grid2>
   )
