b6dc36d6973935e16430524131d5e33d333aaf56
alenko998 <alen_git@hotmail.com>
2025-12-26 21:11:38 +0100

    Creating an activity with a React query mutation

--- client/src/features/activities/form/ActivityForm.tsx
+++ client/src/features/activities/form/ActivityForm.tsx
@@ -8,7 +8,7 @@ type Props = {
 }
 
 export default function ActivityForm({activity, closeForm}: Props) {
-  const {updateActivity} = useActivities();
+  const {updateActivity, createActivity} = useActivities();
 
   const handleSubmit = async (event:FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -22,7 +22,10 @@ export default function ActivityForm({activity, closeForm}: Props) {
       data.id = activity.id;
       await updateActivity.mutateAsync(data as unknown as Activity);
       closeForm();
-    } 
+    } else {
+      await createActivity.mutateAsync(data as unknown as Activity);
+      closeForm();
+    }
   }
 
   return (
@@ -32,12 +35,17 @@ export default function ActivityForm({activity, closeForm}: Props) {
             <TextField name='title' label='Title' defaultValue={activity?.title}/>
             <TextField name='description' label='Description' defaultValue={activity?.description} multiline rows={3} />
             <TextField name='category' label='Category' defaultValue={activity?.category} />
-            <TextField name='date' label='Date' type="date" defaultValue={activity?.date ? new Date(activity.date).toISOString().split('T')[0] : new Date().toISOString().split('T'[0])} />
+            <TextField name='date' label='Date' type="date"
+                    defaultValue={activity?.date
+                        ? new Date(activity.date).toISOString().split('T')[0]
+                        : new Date().toISOString().split('T')[0]
+                    }
+            />
             <TextField name='city' label='City' defaultValue={activity?.city}/>
             <TextField name='venue' label='Venue' defaultValue={activity?.venue}/>
             <Box display='flex' justifyContent='end' gap={3}>
                 <Button onClick={closeForm} color='inherit'>Cancel</Button>
-                <Button disabled={updateActivity.isPending} type="submit" color='success' variant="contained">Submit</Button>
+                <Button disabled={updateActivity.isPending || createActivity.isPending} type="submit" color='success' variant="contained">Submit</Button>
             </Box>
         </Box>
     </Paper>
