bd6a393d739c7cb4302c8b01e6e18e105cf1c337
alenko998 <alen_git@hotmail.com>
2026-01-04 19:52:33 +0100

    Formatting the dates

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af074e2da4c54db4f8a18e4303dfcdc980080d8b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bfb11fa32c1134931cb13bcc424d14c87d028ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
